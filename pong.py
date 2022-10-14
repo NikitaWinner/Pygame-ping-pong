b0cdab5db5d1b6f160d753400edd531112c74bb5
Nikita <kyle.winner@gmail.com>
2022-10-14 15:17:39 +0300

    Добавление мяча и его анимация

--- pong.py
+++ pong.py
@@ -1,5 +1,21 @@
 import pygame, sys
 
+
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+
 # Начальные настройки библиотеки PyGame
 pygame.init()
 clock = pygame.time.Clock()
@@ -7,30 +23,35 @@ clock = pygame.time.Clock()
 # Настройки главного окна
 screen_width = 1280
 screen_height = 600
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
-# Цвета
-light_grey = (200,200,200)
+# Цвета для главного окна
+light_grey = (200, 200, 200)
 bg_color = pygame.Color('grey12')
 
 # Создание прямоугольных областей спрайтов
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10,140)
-opponent = pygame.Rect(10, screen_height / 2 - 70, 10,140)
+player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
+
+ball_speed_x = 7
+ball_speed_y = 7
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	# Отрисовка спрайтов
-	screen.fill(bg_color)
-	pygame.draw.rect(screen, light_grey, player)
-	pygame.draw.rect(screen, light_grey, opponent)
-	pygame.draw.ellipse(screen, light_grey, ball)
-	pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
+    # Анимация мяча
+    ball_animation()
+    # Отрисовка спрайтов поля и спрайтов
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
 
-	pygame.display.flip()
-	clock.tick(60)
+    pygame.display.flip()
+    clock.tick(60)
