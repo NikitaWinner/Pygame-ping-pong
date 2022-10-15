78cd4e1dfaef4417e0ed6ad4b8fa6b722b6deaa1
Nikita <kyle.winner@gmail.com>
2022-10-15 20:17:29 +0300

    Добавление функции рестарта

--- pong.py
+++ pong.py
@@ -10,7 +10,7 @@ def ball_animation():
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
+        ball_start()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
@@ -37,27 +37,36 @@ def opponent_ai():
         opponent.bottom = screen_height
 
 
-# Начальные настройки библиотеки PyGame
+def ball_start():
+    global ball_speed_x, ball_speed_y
+
+    ball.center = (screen_width / 2, screen_height / 2)
+    ball_speed_y *= random.choice((1, -1))
+    ball_speed_x *= random.choice((1, -1))
+
+
+# General setup
 pygame.init()
 clock = pygame.time.Clock()
 
-# Настройки главного окна
+# Main Window
 screen_width = 1280
 screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
-# Цвета для главного окна
+# Colors
 light_grey = (200, 200, 200)
 bg_color = pygame.Color('grey12')
 
-# Создание прямоугольных областей спрайтов
+# Game Rectangles
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height / 2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height / 2 - 70, 10, 140)
 
-ball_speed_x = 7
-ball_speed_y = 7
+# Game Variables
+ball_speed_x = 7 * random.choice((1, -1))
+ball_speed_y = 7 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 7
 
@@ -77,12 +86,12 @@ while True:
             if event.key == pygame.K_DOWN:
                 player_speed -= 6
 
-    # Анимация мяча
+    # Game Logic
     ball_animation()
     player_animation()
     opponent_ai()
 
-    # Отрисовка спрайтов поля и спрайтов
+    # Visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
